753e728bc069d830f39df17381a3511643dad72f
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-08-29 14:20:12 -0400

    pycomm-7

---
+++ pythonbascic/py2.py
@@ -0,0 +1,5 @@
+print('--------------CLASSE----------------\n A classe é o projeto do objeto, contendo o código de programação \n')
+
+print('--------------OBJETO----------------\n É a execução de uma classe, quando executamos o código de uma classe é criado um novo objeto na memória. Uma nova instância de um objeto é um tipo abstrato de informação de um novo tipo de dado \n')
+
+print('--------------INSTÂNCIA----------------\n A instância é o objeto sendo executado, quando criamos um novo objeto, afirmamos qu eestamos criando uma instância dele. \n')
