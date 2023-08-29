49f4099b2c6963c88bbd9d45596b44589f67fcb0
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-08-29 15:09:44 -0400

    pycomm-10

---
+++ pythonbascic/py9.py
@@ -0,0 +1,11 @@
+# Encapsulamento de Dados
+# Agora irei conhecer o conceito de encapsulamento de dados, alterando a visibilidade dos membros e aplicando os métodos Getter e Setter.
+# Uma das principais vantagens do conceito de orientação a objetos é a utilização de estruturas sem a necessidade de conhecer como elas foram implementadas.
+# Para isso, o conceito de encapsulamento de dados torna-se essencial, pois envolve a proteção dos atributos ou métodos de uma classe.
+# A ideia de encapsular o código vem com a premissa de proteger atributos e métodos de uma classe (tornando-os privados), de forma que somente a classe onde as declarações foram feitas tenham acesso.
+
+# Para isso, na linguagem Python temos o que chamamos de convenção para aplicação destes conceitos de orientação a objetos.
+
+# É muito importante ressaltar que a maioria das IDEs (inclusive o PyCharm) oculta, automaticamente, atributos ou classes quando utilizamos a convenção de forma correta.
+
+
